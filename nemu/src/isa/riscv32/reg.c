40f9b9e63c08f45241264f13d539897c61467c8c
tracer-ics2021 <tracer@njuics.org>
2022-02-03 13:03:23 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: 4959b1f16ce64f1aa3785779d5f5b59d   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  13:03:23 up  2:43,  1 user,  load average: 0.94, 0.59, 0.40

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -11,7 +11,7 @@ const char *regs[] = {
 void isa_reg_display() {
 	for( int i = 0; i < 32; i++ )
 	{
-		printf("%-5s %-40u\n", regs[i], cpu.gpr[i]._32);
+		printf("%-5s %-40u 0x %-40x\n", regs[i], cpu.gpr[i]._32, cpu.gpr[i]._32);
 	}
 }
 
