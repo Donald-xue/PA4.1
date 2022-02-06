e5d18a28855a10d04467bf145f3512d20d186473
tracer-ics2021 <tracer@njuics.org>
2022-02-06 22:51:52 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: d2adf63cb3b24fc38fc8648f3177a376   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  22:51:52 up  2:38,  1 user,  load average: 0.20, 0.14, 0.16

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -128,8 +128,8 @@ static bool make_token(char *e) {
         break;
       }
     }
-   // for (int j = 0; j < nr_token; j++)
-     //      printf("type=%d, str=%s\n", tokens[j].type, tokens[j].str);
+    for (int j = 0; j < nr_token; j++)
+           printf("type=%d, str=%s\n", tokens[j].type, tokens[j].str);
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
