8e33a072d22b3ed10e36eb043698abc1fcf47ddd
tracer-ics2021 <tracer@njuics.org>
2022-02-07 00:06:25 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: d2adf63cb3b24fc38fc8648f3177a376   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  00:06:25 up  3:53,  1 user,  load average: 0.30, 0.23, 0.16

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -193,13 +193,8 @@ int eval(int p, int q) {
 			  num--;
 		  if (num == 0){
 			  if (tokens[j].type == '+' || '-' || '*' || '/'){
-				  if(tokens[j].type == '+'){
+				  if(tokens[j].type == '+' || '-'){
 					  operator = j;
-					  printf("add\n");
-				  }
-				  else if (tokens[j].type == '-'){
-					  operator = j;
-				      printf("minus\n");
 				  }
 				  //else if (operator == -1)
 					//  operator = j;
