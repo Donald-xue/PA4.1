be3af8873351da22db07c9e03a55e6f908515c89
tracer-ics2021 <tracer@njuics.org>
2022-02-09 23:49:13 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: 8b33ad977ef24b97b56947e4dafb703a   Operating System: Ubuntu 20.04.3 LTS             Kernel: Linux 5.11.0-27-generic       Architecture: x86-64  23:49:13 up  3:34,  1 user,  load average: 0.22, 0.29, 0.24

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -49,6 +49,9 @@ void gen_rand_op(){
 static void gen_rand_expr() {
   buf[0] = '\0';
   switch(choose(3)){
+	  case 0: gen_num(); break;
+	  case 1: gen('('); gen_rand_expr(); gen(')'); break;
+	  default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
   }
 }
 
