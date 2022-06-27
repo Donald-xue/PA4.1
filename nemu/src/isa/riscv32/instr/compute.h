1ce16081df040de8ae8ce248be4ec0e8de2b85ec
tracer-ics2021 <tracer@njuics.org>
2022-06-27 14:53:47 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: ee3f1b66858044628ee0a6a25225ac58   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  14:53:47 up 1 day,  4:33,  1 user,  load average: 0.23, 0.23, 0.19

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -27,9 +27,9 @@ def_EHelper(csrrw){
 def_EHelper(csrrs){
 	if(id_src2->imm == 834){
         unsigned int t = cpu.mcause;
-		printf("!!!!!!!!cpu.mcause = %x, dsrc1 = %x\n", cpu.mcause, *dsrc1);
+//		printf("!!!!!!!!cpu.mcause = %x, dsrc1 = %x\n", cpu.mcause, *dsrc1);
         cpu.mcause = t | *dsrc1;
-      printf("!!!!!!!!cpu.mcause = %x", cpu.mcause);
+//      printf("!!!!!!!!cpu.mcause = %x", cpu.mcause);
         *ddest = t;
     }
 	if(id_src2->imm == 768){
