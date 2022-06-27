fde08e61fc4153b5e731703252c59c77a490477b
tracer-ics2021 <tracer@njuics.org>
2022-06-27 12:16:24 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: ee3f1b66858044628ee0a6a25225ac58   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  12:16:24 up 1 day,  1:56,  1 user,  load average: 0.18, 0.14, 0.13

--- nemu/src/isa/riscv32/instr/jump.h
+++ nemu/src/isa/riscv32/instr/jump.h
@@ -7,6 +7,7 @@ extern word_t isa_raise_intr(word_t NO, vaddr_t epc);
 def_EHelper(ecall) {
 //	printf("!!!!!!!!!pc = %x", cpu.pc);
 	isa_raise_intr(cpu.gpr[17]._32, cpu.pc);
+	printf("!!!!!!!!!pc = %x", cpu.pc);
 }
 
 def_EHelper(bne) {
