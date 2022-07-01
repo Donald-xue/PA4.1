1b2c9cbf64e298c18e68e4e37c11e7d8619d770e
tracer-ics2021 <tracer@njuics.org>
2022-07-01 16:47:52 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: ee3f1b66858044628ee0a6a25225ac58   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  16:47:52 up 5 days,  6:27,  1 user,  load average: 0.58, 0.58, 0.41

--- nemu/src/isa/riscv32/instr/compute.h
+++ nemu/src/isa/riscv32/instr/compute.h
@@ -23,15 +23,13 @@ def_EHelper(csrrw){
         *ddest = t;
     }
 }
-/*
-#ifdef CONFIG_TRACE
+
 void exctrace(uint32_t cause){
 	switch(cause){
             case 0xffffffff: etrace_write("Get an EVENT_YIELD!\n");  break;
             default: etrace_write("Undefined mcause in etrace!\n"); break;
         }
 }
-#endif
 
 def_EHelper(csrrs){
 	if(id_src2->imm == 834){
@@ -41,11 +39,11 @@ def_EHelper(csrrs){
 //      printf("!!!!!!!!cpu.mcause = %x", cpu.mcause);
         *ddest = t;
 #ifdef CONFIG_TRACE
-		switch(cpu.mcause){
+/*		switch(cpu.mcause){
 			case 0xffffffff: etrace_write("Get an EVENT_YIELD!\n"); break;
 			default: etrace_write("Undefined mcause in etrace!\n"); break;
-		}
-//		exctrace(cpu.mcause);
+		}*/
+		exctrace(cpu.mcause);
 #endif
     }
 	if(id_src2->imm == 768){
@@ -61,7 +59,7 @@ def_EHelper(csrrs){
         *ddest = t;
     }
 }
-*/
+
 def_EHelper(xori){
 	rtl_xori(s, ddest, dsrc1, id_src2->imm);
 }
