c444c770365adc6f3c3fc23de1b7015693442346
tracer-ics2021 <tracer@njuics.org>
2022-07-06 17:27:16 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: b64e0fadb7e34315b9eb18be1d398f40   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  17:27:16 up  7:26,  1 user,  load average: 0.47, 0.48, 0.39

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -18,7 +18,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
   Elf_Ehdr *elf = &ehdr;
-  printf("魔数为: %d\n", *(uint32_t *)elf->e_ident);
+  printf("魔数为: %x\n", *(uint32_t *)elf->e_ident);
 //  assert(*(uint32_t *)elf->e_ident == 0x7f454c46);
   Elf_Phdr phdr[ehdr.e_phnum];
   ramdisk_read(phdr, ehdr.e_ehsize, ehdr.e_phentsize);
