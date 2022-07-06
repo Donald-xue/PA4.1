74fa8f6e68d5e4ffbd4c17c7eb48fbc23228c697
tracer-ics2021 <tracer@njuics.org>
2022-07-06 19:04:12 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: b64e0fadb7e34315b9eb18be1d398f40   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  19:04:12 up  9:03,  1 user,  load average: 0.60, 0.47, 0.36

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,16 +13,31 @@ extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 extern size_t get_ramdisk_size();
 
+#if defined(__ISA_AM_NATIVE__)
+# define EXPECT_TYPE EM_X86_64
+#elif defined(__ISA_X86__)
+# define EXPECT_TYPE EM_X86_64  // see /usr/include/elf.h to get the right type
+#elif defined(__ISA_RISCV32__)
+# define EXPECT_TYPE EM_RISCV
+#elif defined(__ISA_MIPS32__)
+# define EXPECT_TYPE EM_MIPS
+#else
+# error Unsupported ISA
+#endif
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
 //  TODO();
   Elf_Ehdr ehdr;
   ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
   Elf_Ehdr *elf = &ehdr;
   assert(*(uint32_t *)elf->e_ident == 0x464c457f);
+  printf("EXPECT_TYPE = %d, ehdr.e_machine = %d\n", EXPECT_TYPE, ehdr.e_machine);
+  assert(EXPECT_TYPE == ehdr.e_machine);
   Elf_Phdr phdr[ehdr.e_phnum];
   ramdisk_read(phdr, ehdr.e_ehsize, ehdr.e_phentsize);
   for(int i = 0; i < ehdr.e_phnum; i++){
 	  if( phdr[i].p_type == PT_LOAD ){
+		  printf("第%d个信息!!\n", i);
 		  ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);
 		  memset((void *)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz-phdr[i].p_filesz);
 	  }
