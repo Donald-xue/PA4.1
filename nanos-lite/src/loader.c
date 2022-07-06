6f537924bb652eda374b62b8e568d64627bd2237
tracer-ics2021 <tracer@njuics.org>
2022-07-06 16:23:44 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: b64e0fadb7e34315b9eb18be1d398f40   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-51-generic       Architecture: x86-64  16:23:44 up  6:23,  1 user,  load average: 0.22, 0.35, 0.37

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,9 +9,23 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+extern size_t get_ramdisk_size();
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+//  TODO();
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  Elf_Phdr phdr[ehdr.e_phnum];
+  ramdisk_read(phdr, ehdr.e_ehsize, ehdr.e_phentsize);
+  for(int i = 0; i < ehdr.e_phnum; i++){
+	  if( phdr[i].p_type == PT_LOAD ){
+		  ramdisk_write((void *)phdr[i].p_offset, phdr[i].p_vaddr, phdr[i].p_filesz);
+		  memset((void *)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz-phdr[i].p_filesz);
+	  }
+  }
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
