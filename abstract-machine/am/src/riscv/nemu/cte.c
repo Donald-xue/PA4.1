a12e8cc37457cbe979d8c8f6d10759a3a1c59098
tracer-ics2021 <tracer@njuics.org>
2022-07-21 17:23:16 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: 733103f51d5946e3a109a16762401695   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-52-generic       Architecture: x86-64  17:23:17 up  5:16,  1 user,  load average: 0.86, 0.46, 0.31

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -9,12 +9,32 @@ Context* __am_irq_handle(Context *c) {
 		printf("gpr(%d) = %x\n", i, c->gpr[i]);
 	}
 */	
-	printf("%x %x %x\n", c->mcause, c->mstatus, c->mepc);
+//	printf("%x %x %x\n", c->mcause, c->mstatus, c->mepc);
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
 		case 0xffffffff: ev.event = EVENT_YIELD; break;
-		case 1: ev.event = EVENT_SYSCALL; break;
+		case 0:
+		case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:    
+		case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+        case 16:
+        case 17:
+        case 18:
+        case 19:
+						 ev.event = EVENT_SYSCALL; break;
 		default: ev.event = EVENT_ERROR; break;
     }
     c = user_handler(ev, c);
