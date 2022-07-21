3c8f88087487c97e377d031a0cba60a1dbf7ba78
tracer-ics2021 <tracer@njuics.org>
2022-07-21 16:56:28 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: 733103f51d5946e3a109a16762401695   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-52-generic       Architecture: x86-64  16:56:28 up  4:49,  1 user,  load average: 0.17, 0.33, 0.34

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,6 +3,31 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
 	  case EVENT_YIELD: printf("Get an EVENT_YIELD!\n"); break;
+	  case EVENT_SYSCALL: 
+						switch (c->gpr[17]){
+							case 0:
+							case 1:
+							case 2:
+							case 3:
+							case 4:
+							case 5:
+							case 6:
+							case 7:
+							case 8:
+							case 9:
+							case 10:
+							case 11:
+							case 12:
+							case 13:
+							case 14:
+							case 15:
+							case 16:
+							case 17:
+							case 18:
+							case 19:
+							   printf("Get an EVENT_SYSCALL!\n");	
+						}
+						break;
 	  default: panic("Unhandled event ID = %d", e.event);
   }
 
