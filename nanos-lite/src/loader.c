e3bfd993d08b1234f03b9c010116a9288dbd46d3
tracer-ics2021 <tracer@njuics.org>
2022-07-22 16:57:19 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: 733103f51d5946e3a109a16762401695   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-52-generic       Architecture: x86-64  16:57:19 up 1 day,  4:50,  1 user,  load average: 1.10, 0.89, 0.77

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -34,11 +34,11 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 //  printf("EXPECT_TYPE = %d, ehdr.e_machine = %d\n", EXPECT_TYPE, ehdr.e_machine);
   assert(EXPECT_TYPE == ehdr.e_machine);
   Elf_Phdr phdr[ehdr.e_phnum];
-  ramdisk_read(phdr, ehdr.e_ehsize, ehdr.e_phentsize);
+  ramdisk_read(phdr, ehdr.e_ehsize, sizeof(Elf_Phdr)*ehdr.e_phnum);
    for(int i = 0; i < ehdr.e_phnum; i++){
 	  if( phdr[i].p_type == PT_LOAD ){
 //		  printf("第%d个信息!!\n", i);
-		  ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_filesz);
+		  ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
 		  memset((void *)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz-phdr[i].p_filesz);
  	  }
   }
--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -41,7 +41,7 @@
 #endif
 
 intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
-  printf("Into the syscall!\n");
+//  printf("Into the syscall!\n");
   register intptr_t _gpr1 asm (GPR1) = type;
   register intptr_t _gpr2 asm (GPR2) = a0;
   register intptr_t _gpr3 asm (GPR3) = a1;
@@ -54,7 +54,7 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
 }
 
 void _exit(int status) {
-//  _syscall_(SYS_exit, status, 0, 0);
+  _syscall_(SYS_exit, status, 0, 0);
   while (1);
 }
 /*
