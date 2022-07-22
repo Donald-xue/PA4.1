b21a634b2bb4fc561bf82652786483e77e6d979c
tracer-ics2021 <tracer@njuics.org>
2022-07-22 18:08:54 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: 733103f51d5946e3a109a16762401695   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.13.0-52-generic       Architecture: x86-64  18:08:54 up 1 day,  6:01,  1 user,  load average: 0.30, 0.28, 0.34

--- nemu/include/utils.h
+++ nemu/include/utils.h
@@ -99,8 +99,8 @@ uint64_t get_time();
 #define strace_write(...) IFDEF(CONFIG_TARGET_NATIVE_ELF, \
         do {\
         extern FILE* s_fp; \
-        extern bool s_enable(); \
-        if (s_enable()) { \
+        extern bool log_enable(); \
+        if (log_enable()) { \
         fprintf(s_fp, __VA_ARGS__); \
         fflush(s_fp);\
          }\
