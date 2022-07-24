01e395c0c7f948df2abd603674f507e18d29b86f
tracer-ics2021 <tracer@njuics.org>
2022-07-24 17:16:59 +0800

    >  compile 03018113    Static hostname: xzq          Icon name: computer-laptop            Chassis: laptop         Machine ID: 4f6b4c0514ea4b59ac45ff049f1c5c9c            Boot ID: f0a742e1230747518552723e2d3cf3c6   Operating System: Ubuntu 20.04.4 LTS             Kernel: Linux 5.15.0-41-generic       Architecture: x86-64  17:16:59 up  1:04,  1 user,  load average: 0.50, 0.40, 0.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -8,15 +8,33 @@ int sys_yield(Context *c){
 	return 0;
 }
 
+int sys_write(int fd, void *buf, size_t count, Context *c){
+	if(fd == 1 || fd == 2){
+		for(int i = 0; i < count; i++){
+			putch(*(char*)buf);
+			(char *)buf++;
+		}
+		c->GPRx = count;
+		return count;
+	}
+	c->GPRx = -1;
+	return -1;
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
+  a[1] = c->GPR2;
+  a[2] = c->GPR3;
+  a[3] = c->GPR4;
 
   switch (a[0]) {
 	  case 0: printf("Get an SYS_exit!\n");
 		  halt(c->GPRx); break;
 	  case 1: printf("Get an SYS_yield!\n");
 			  sys_yield(c);  break;
+	  case 5: printf("Get an SYS_write!\n");
+			  sys_write(a[1], (void *)a[2], a[3], c);
       default: panic("Unhandled syscall ID = %d", a[0]);
   }
 //  printf("Finished do_syscall!\n");
