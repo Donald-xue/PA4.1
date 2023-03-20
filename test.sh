19073b7f9d7960d143a0da10f515dbc4bc1d30a4
Xue Zeqian <xuezeqian0426@163.com>
2023-03-20 09:43:51 +0800

    4.1.2

---
+++ test.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+bash ./init.sh nemu
+bash ./init.sh abstract-machine
+bash ./init.sh fceux-am
+bash ./init.sh am-kernels
+bash ./init.sh nanos-lite
+bash ./init.sh navy-apps
+
+cd navy-apps/apps 
+cd nterm
+make ISA=riscv32 install
+cd ..
+cd bird
+make ISA=riscv32 install
+cd ..
+cd menu
+make ISA=riscv32 install
+cd ..
+cd pal
+make ISA=riscv32 install
+cd ..
+cd nslider
+make ISA=riscv32 install
+cd ..
+cd busybox
+make ISA=riscv32 install
+cd ..
+cd ../..
+
+cd nanos-lite
+make ARCH=riscv32-nemu update
+make ARCH=riscv32-nemu run
